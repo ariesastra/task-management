69551f797ee0dd92b2961dde48dba0d75ae4d467
ariesastra <ariesastra@windowslive.com>
2022-05-16 14:24:17 +0700

    update task controller

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -1,4 +1,9 @@
 import { Controller } from '@nestjs/common';
+import { TaskService } from './task.service';
 
 @Controller('task')
-export class TaskController {}
+export class TaskController {
+  constructor(
+    private taskService: TaskService
+  ) {}
+}
