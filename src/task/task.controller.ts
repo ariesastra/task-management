168c58237a8a84016a8b3fa59b7d4df199cbfc3b
ariesastra <ariesastra@windowslive.com>
2022-06-16 17:17:27 +0700

    update

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -1,9 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Task } from './task.model';
 import { TaskService } from './task.service';
+import { createTaskDto } from './task.dto';
 
 @Controller('task')
 export class TaskController {
   constructor(
-    private taskService: TaskService
+    private readonly taskService: TaskService
   ) {}
+
+  @Get()
+  getAllTask(): Task[] {
+    return this.taskService.getAllTask();
+  }
+
+  @Post()
+  createTask(
+    @Body() createTaskDto: createTaskDto
+  ): Task {
+    return this.taskService.createTask(createTaskDto.title, createTaskDto.description);
+  }
 }
---
+++ src/task/task.dto.ts
@@ -0,0 +1,4 @@
+export interface createTaskDto {
+  title: string;
+  description: string;
+}
